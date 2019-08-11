48dbc7d1d16417968bd2871bd38b85279b1abf55
Tal Leibovitz <t-taleib@microsoft.com>
2019-08-11 13:10:08 +0300

    module5

--- src/data.js
+++ src/data.js
@@ -1,15 +1,9 @@
 const fs = require('fs');
-
 const path = require('path');
-
 const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
-
 const accounts = JSON.parse(accountData);
-
 const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
-
 const users = JSON.parse(userData);
-
 const writeJSON = () => {
 
     const accountsJSON = JSON.stringify(accounts, null, 4);
